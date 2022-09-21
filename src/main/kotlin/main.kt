fbd33488fdeb6f6026eea9714ebad680a56853a5
BexultanN <200107071@stu.sdu.edu.kz>
2022-09-21 10:19:15 +0600

    decoration2 with new line

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,5 @@
 package example.myapp
-
 import example.myapp.decor.makeDecorations
-
-fun makeFish() {
-    val shark = Shark()
-    val pleco = Plecostomus()
-    println("Shark: ${shark.color}")
-    shark.eat()
-    println("Plecostomus: ${pleco.color}")
-    pleco.eat()
-}
 
 fun main () {
     makeDecorations()
