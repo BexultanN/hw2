fb27a634579921198b5c910761b65606bed02227
BexultanN <200107071@stu.sdu.edu.kz>
2022-09-21 09:41:41 +0600

    new constructor

--- src/main/kotlin/Aquarium.kt
+++ src/main/kotlin/Aquarium.kt
@@ -1,11 +1,19 @@
 package example.myapp
 
 class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init{
+        println("==> aquarium initializing")
+    }
     init {
         // 1 liter = 1000 cm^3
         println("Volume: ${width * length * height / 1000} liters")
     }
 
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
 
     fun printSize() {
         println(
