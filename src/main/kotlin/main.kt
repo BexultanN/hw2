ff1bf8828cd833987f569a3940de52cd7ec795f9
BexultanN <200107071@stu.sdu.edu.kz>
2022-09-21 09:35:23 +0600

    buildAquarium with printSize

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,10 +4,17 @@ private fun main() {
     buildAquarium()
 }
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    // default height and length
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    // default width
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    // everything custom
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
 }
 
 
