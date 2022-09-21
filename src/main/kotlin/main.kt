5325981f3bdcad70e053a4a6b4b513249279864b
BexultanN <200107071@stu.sdu.edu.kz>
2022-09-21 10:28:40 +0600

    hashmap 2 yourself

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,26 @@
 package example.myapp
 
-import example.myapp.decor.Choice
+fun main(args: Array<String>) {
 
-fun main() {
-    println(Choice.name)
-    Choice.showDescription("pick")
-    Choice.showDescription("selection")
+    var hashMap : HashMap<String, Int>
+            = HashMap<String, Int> (4)
+
+    hashMap.put("IronMan" , 3000)
+    hashMap.put("Thor" , 100)
+    hashMap.put("SpiderMan" , 1100)
+    hashMap.put("NickFury" , 1200)
+
+    for(key in hashMap.keys) {
+        println("Element at key $key : ${hashMap[key]}")
+    }
+    println("\n" + "hashMap.size : " + hashMap.size )
+
+    hashMap["BlackWidow"] = 1000;
+    println("hashMap.size : " + hashMap.size + "\n")
+
+    for(key in hashMap.keys) {
+        println("Element at key $key : ${hashMap[key]}")
+    }
 }
 
 
