071d88bc23807043ccc800accc467dcb15914514
BexultanN <200107071@stu.sdu.edu.kz>
2022-09-21 09:59:44 +0600

    open class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,10 +4,11 @@ private fun main() {
     buildAquarium()
 }
 fun buildAquarium() {
-    val aquarium6 = Aquarium(numberOfFish = 29)
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
-    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 }
+
+
 
 
 
